7dc8b9b5788feadf997495a5564857e9e10a8d16
lanthora <lanthora@outlook.com>
2024-04-06 23:28:05 +0800

    test: windows dll test

--- main.cc
+++ main.cc
@@ -1,8 +1,12 @@
+#include "candy.h"
 #include "mainwindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    void *candy = candy_client_create();
+    candy_client_release(candy);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
