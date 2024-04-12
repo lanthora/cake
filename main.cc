31c1d7f216f2c4ab519b671f130fd7504aff0d94
lanthora <lanthora@outlook.com>
2024-04-12 14:27:46 +0800

    feat: remove single app limit

--- main.cc
+++ main.cc
@@ -1,18 +1,9 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <QMessageBox>
-#include <QSharedMemory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QSharedMemory shared("canets.org/cake");
-    if (shared.attach()) {
-        QMessageBox::warning(nullptr, "组网工具", "已经有一个实例正在运行");
-        return 0;
-    }
-    shared.create(1);
-
     MainWindow w;
     w.show();
     return a.exec();
