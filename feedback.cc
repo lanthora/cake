47a8ae65f20ad701fe11bc1ebad4170a8f58ab5f
lanthora <lanthora@outlook.com>
2024-05-29 11:48:16 +0800

    fix: feedback label open external link

--- feedback.cc
+++ feedback.cc
@@ -8,17 +8,27 @@ Feedback::Feedback(QWidget *parent)
     setWindowTitle("问题反馈");
     setFixedSize(500, 300);
 
-    QLabel *label = new QLabel(
-        "<b>QQ:</b><br>768305206<br><br>"
-        "<b>Telegram:</b><br><a href=\"https://t.me/CandyUserGroup\">Click to Join</a><br><br>"
-        "<b>Github:</b><br><a href=\"https://github.com/lanthora/cake/issues/new\">Create an issue</a><br>",
+    QLabel *qq = new QLabel(
+        "<b>QQ:</b><br>"
+        "768305206<br>",
         this);
+    qq->setTextInteractionFlags(Qt::TextSelectableByMouse);
 
-    label->setAlignment(Qt::AlignLeft);
-    label->setOpenExternalLinks(true);
-    label->setTextInteractionFlags(Qt::TextSelectableByMouse);
+    QLabel *telegram = new QLabel(
+        "<b>Telegram:</b><br>"
+        "<a href=\"https://t.me/CandyUserGroup\">Click to Join</a><br>",
+        this);
+    telegram->setOpenExternalLinks(true);
+
+    QLabel *github = new QLabel(
+        "<b>Github:</b><br>"
+        "<a href=\"https://github.com/lanthora/cake/issues/new\">Create an issue</a>",
+        this);
+    github->setOpenExternalLinks(true);
 
     QVBoxLayout *layout = new QVBoxLayout(this);
-    layout->addWidget(label, 0, Qt::AlignCenter);
+    layout->addWidget(qq, 0, Qt::AlignLeft);
+    layout->addWidget(telegram, 0, Qt::AlignLeft);
+    layout->addWidget(github, 0, Qt::AlignLeft);
     layout->setAlignment(Qt::AlignCenter);
 }
