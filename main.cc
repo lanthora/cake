5695d5a7202355e6c7990e33acad327e8024aab2
lanthora <lanthora@outlook.com>
2025-05-30 10:49:53 +0800

    feat: windows coredump

--- main.cc
+++ main.cc
@@ -1,13 +1,32 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDir>
 #include <QMessageBox>
 #include <QSharedMemory>
+
+#ifdef Q_OS_WIN
+void configureWerDumpSettings()
+{
+    const QString dumpPath = "C:/ProgramData/Cake/dmps/";
+    QDir().mkpath(dumpPath);
+    QString regPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\Windows Error Reporting\\LocalDumps\\cake.exe";
+    QSettings werSettings(regPath, QSettings::NativeFormat);
+    werSettings.setValue("DumpFolder", dumpPath);
+    werSettings.setValue("DumpCount", 3);
+    werSettings.setValue("DumpType", 2);
+    werSettings.sync();
+}
+#endif
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setOrganizationName("canets");
     QCoreApplication::setOrganizationDomain("canets.org");
     QCoreApplication::setApplicationName("cake");
+
+#ifdef Q_OS_WIN
+    configureWerDumpSettings();
+#endif
 
     QApplication a(argc, argv);
     QSharedMemory shared("canets.org/cake");
